eaa28d7e4c0b67e87cc8e27b881639bd65c85e1c
David Riesz <dbriesz@users.noreply.github.com>
2017-02-17 13:09:31 -0800

    Fixed application failed to start bug, added Thymeleaf prefix to CSS links

--- src/main/java/com/teamtreehouse/instateam/dao/ProjectDaoImpl.java
+++ src/main/java/com/teamtreehouse/instateam/dao/ProjectDaoImpl.java
@@ -4,9 +4,11 @@ import com.teamtreehouse.instateam.model.Project;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class ProjectDaoImpl implements ProjectDao {
     @Autowired
     private SessionFactory sessionFactory;
