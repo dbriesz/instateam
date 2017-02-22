252975bfbc327c003ddd46d43555438d92676e31
David Riesz <dbriesz@users.noreply.github.com>
2017-02-22 12:35:19 -0800

    Added ability to add new projects

--- src/main/java/com/teamtreehouse/instateam/web/controller/ProjectController.java
+++ src/main/java/com/teamtreehouse/instateam/web/controller/ProjectController.java
@@ -1,7 +1,9 @@
 package com.teamtreehouse.instateam.web.controller;
 
 import com.teamtreehouse.instateam.model.Project;
+import com.teamtreehouse.instateam.model.Role;
 import com.teamtreehouse.instateam.service.ProjectService;
+import com.teamtreehouse.instateam.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,12 +11,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 public class ProjectController {
     @Autowired
     private ProjectService projectService;
+
+    @Autowired
+    private RoleService roleService;
 
     // Home page - index of all projects
     @RequestMapping("/")
@@ -23,6 +29,7 @@ public class ProjectController {
         List<Project> projects = projectService.findAll();
 
         model.addAttribute("projects", projects);
+
         return "index";
     }
 
@@ -30,7 +37,7 @@ public class ProjectController {
     @RequestMapping("/projects/{projectId}")
     public String project(@PathVariable Long projectId, Model model) {
         // Get the project given by projectId
-        Project project = null;
+        Project project = projectService.findById(projectId);
 
         model.addAttribute("project", project);
         return "project/details";
@@ -39,8 +46,11 @@ public class ProjectController {
     // Form for adding a new project
     @RequestMapping("projects/add")
     public String formNewProject(Model model) {
-        // TODO: Add model attributes needed for new form
+        // Add model attributes needed for new form
         model.addAttribute("project", new Project());
+
+        model.addAttribute("role", new Role());
+        model.addAttribute("roles", roleService.findAll());
 
         return "project/index";
     }
@@ -50,7 +60,7 @@ public class ProjectController {
     public String editProject(@PathVariable Long projectId, Model model) {
         // TODO: Add model attributes needed for edit form
 
-        return "project/form";
+        return "project/index";
     }
 
     // Update an existing project
@@ -63,13 +73,13 @@ public class ProjectController {
     }
 
     // Add a project
-    @RequestMapping(value = "/projects", method = RequestMethod.POST)
-    public String addProject(Project project) {
+    @RequestMapping(value = "/projects/add", method = RequestMethod.POST)
+    public String addProject(@Valid Project project) {
         // TODO: Add project if valid data was received
         projectService.save(project);
 
         // TODO: Redirect browser to home page
-        return "redirect:/projects";
+        return "redirect:/";
     }
 
     // Delete an existing project
